820b0856ecdd33fb2a40404ebc54ff72208a382c
antonio allen <modusoul@gmail.com>
2020-08-13 19:20:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,15 @@ function add(a,b)
 number = a + b
 return a + b
 }
+
+function subtract(a,b)
+{
+  number = a - b
+  return a - b
+}
+
+function multiply(a,b)
+{
+  number = a * b
+  return a * b
+}
